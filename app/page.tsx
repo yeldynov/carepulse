938967a4c4d6aa7890407aea589f26c357700b7f
Nikita Yeldynov <yeldynov@gmail.com>
2024-08-13 14:25:00 +0300

    + admin passkey modal

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,28 @@
 import PatientForm from '@/components/forms/PatientForm'
-import { Button } from '@/components/ui/button'
+import PasskeyModal from '@/components/PasskeyModal'
 import Image from 'next/image'
 import Link from 'next/link'
 
-export default function Home() {
+export default function Home({ searchParams }: SearchParamProps) {
+  const isAdmin = searchParams.admin === 'true'
+
   return (
     <div className='flex h-screen max-h-screen'>
-      {/* TODO: OTP Verification | PasskeyModal */}
+      {isAdmin && <PasskeyModal />}
 
-      <section className='remove-scrollbar container my-auto'>
+      <section className='container my-auto remove-scrollbar'>
         <div className='sub-container max-w-[496px]'>
           <Image
             src='/assets/icons/logo-full.svg'
             height={1000}
             width={1000}
             alt='patient'
-            className='mb-12 h-10 w-fit'
+            className='h-10 mb-12 w-fit'
           />
 
           <PatientForm />
 
-          <div className='text-14-regular mt-20 flex justify-between'>
+          <div className='flex justify-between mt-20 text-14-regular'>
             <p className='justify-items-end text-dark-600 xl:text-left'>
               © 2024 CarePulse
             </p>
