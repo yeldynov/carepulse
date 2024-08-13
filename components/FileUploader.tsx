6e799291e357337e9984340650c2bfc1e1d61027
Nikita Yeldynov <yeldynov@gmail.com>
2024-08-13 12:14:48 +0300

    fixed deployment error

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -47,7 +47,7 @@ const FileUploader = ({ files, onChange }: FileUploaderProps) => {
       {isDragActive ? (
         <p>Drop the files here ...</p>
       ) : (
-        <p>Drag 'n' drop some files here, or click to select files</p>
+        <p>Drag n drop some files here, or click to select files</p>
       )}
     </div>
   )
